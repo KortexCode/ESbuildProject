b585b3f8b2b8442ee11ff6f68cd94873cdf68d77
CortesCode <cacortesl01@gmail.com>
2023-01-03 14:48:12 -0500

    se termina la lógica en index.tsx, se agregan archivos de configuración y scripts para hacer build y activar el servidor local.

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -3,11 +3,15 @@
 import React, {useCallback, useState} from 'react';
 
 const App = (props: {message:string})=>{
+    const [count, setCount] = useState(0);
+    const Increments = useCallback(()=>{
+        setCount((count)=> count + 1);
+    }, [count]);
     return(
         <>
             <h1>{props.message}</h1>
             <h2>Count:{count}</h2>
-            <button>Increments</button>
+            <button onClick={Increments} >Increments</button>
         </>
     )
 }
